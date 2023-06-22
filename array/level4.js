82e9acebecb2f9a0da473c1ecac0d9fdc9691542
obedlama <obedlama6@gmail.com>
2023-06-22 09:12:48 +0545

    changed array

--- array/level4.js
+++ array/level4.js
@@ -3,3 +3,16 @@
 //true
 const name= 'hari'
 const allUsers = ['hari','shyam','thakur']
+function compare()
+{
+ for(i=0; i<allUsers.length; i++)
+ {
+    if(allUsers[i]==name)
+    
+    {
+        console.log('name is found')
+    }
+
+ }
+}
+compare()
