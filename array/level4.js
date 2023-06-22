cce421c5f651089e017ed199a6e14a0834e19038
obedlama <obedlama6@gmail.com>
2023-06-22 18:00:26 +0545

    finding duplicates in an array

--- array/level4.js
+++ array/level4.js
@@ -16,3 +16,18 @@ function compare()
  }
 }
 compare()
+
+//Q2 find number of duplicates
+//expected output is : 2
+//true
+
+const array = ['hari','shyam','hari','thakur']
+let count = 0
+for (let i = 0; i < array.length; i++) {
+  for (let j = i +1; j < array.length; j++) {
+    if (array[i] === array[j] ) {
+      count = count + 2
+    }
+  }
+}
+console.log(count)
