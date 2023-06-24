a7b7f40f18f27d83dd763bc3dc8f2710880b5426
obedlama <obedlama6@gmail.com>
2023-06-24 23:18:34 +0545

    using for of and finding unique

---
+++ array/level5.js
@@ -0,0 +1,17 @@
+const arr=[2,3,6,7]
+let sum = 0
+for(let i of arr){
+ sum = sum + i
+}
+console.log(i)
+
+const users = ['hari','shyam','hari']
+let temArr=[]
+users.map((item,id)=> {
+  if(!temArr.includes(item)){
+    temArr.push(item)
+  }
+})
+temArr
+
+
