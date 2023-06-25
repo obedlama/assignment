616b7392fbf0a72611b9003104edcad8d2a658ce
obedlama <obedlama6@gmail.com>
2023-06-25 09:28:13 +0545

    high level array problems

--- array/level5.js
+++ array/level5.js
@@ -1,3 +1,4 @@
+//calculate the sum of the array using either for of or for in
 const arr=[2,3,6,7]
 let sum = 0
 for(let i of arr){
@@ -5,6 +6,7 @@ for(let i of arr){
 }
 console.log(i)
 
+//remove from array if duplicate
 const users = ['hari','shyam','hari']
 let temArr=[]
 users.map((item,id)=> {
@@ -13,5 +15,24 @@ users.map((item,id)=> {
   }
 })
 temArr
+//calculate the sum of all the odd numbers inside this nested array
+const newArr = [[4,5], [5,7], [7,2]]
+let sum = 0
+newArr.flat().map((item)=>{
+  if(item%2!=0){
+    sum = sum + item
+  }
+ })
+ console.log(sum)
 
+// return only array of ids: expected output  [3,5,6]
+const myDetails = [
+      {id:3, name: 'hari'},
+      {id:5, name: 'shyam'},
+      {id:6, name: 'gopal'},
+    ]
+   myDetails.map((item)=>{
+    return item.id
+   })
+   console.log(item)
 
