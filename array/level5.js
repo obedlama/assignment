85d3d81b5a77a068cb40d0672e6c3afc633bd551
obedlama <obedlama6@gmail.com>
2023-06-28 00:21:11 +0545

    problems relating array

--- array/level5.js
+++ array/level5.js
@@ -36,3 +36,19 @@ const myDetails = [
    })
    console.log(item)
 
+   const userDetails= [
+    {score: 0, name:'hari', marks: [10,3,23]},
+    {score: 0, name:'shyam', marks: [50,23,23]},
+    {score: 0, name:'shyam',marks: [20,13,43]},
+  ]
+  //loop over the arr of objects and calculate total score, expected output is:
+  
+  userDetails.map((item)=>{
+    item.score = item.marks[0]+item.marks[1]+item.marks[2];
+    console.log(item);
+  }
+  )
+  //     {score: 36, name:'hari', marks: [10,3,23]},
+  //     {score: 96, name:'shyam', marks: [50,23,23]},
+  //     {score: 76, name:'shyam',marks: [20,13,43]},
+  
